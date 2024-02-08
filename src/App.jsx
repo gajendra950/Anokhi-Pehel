c2a83fd9081bcfac68dfeb3db4dd383c43e4e198
Gajendra <yaduvanshigajendra2002@gmail.com>
2024-02-08 17:57:05 +0530

    Added Events pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import Homepage from "./pages/Homepage";
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage"
+// import './App.css'
+import Events from "./components/Events"
 export default function App() {
   return (
-    <Homepage/>
+    <>
+     <BrowserRouter>
+     <Routes>
+     <Route
+      path ="/"
+      element={
+        <HomePage/>
+      }
+     />
+      <Route
+      path ="/event"
+      element={
+        <Events/>
+      }
+     />
+     </Routes>
+     </BrowserRouter>
+    </>
   )
 }
-        
---
+++ src/components/Events.jsx
@@ -0,0 +1,102 @@
+  import Antodaya from "../assets/antyodaya.jpg"
+  export default function CardDefault() {
+    return (
+        <>
+        <div className="flex justify-center">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src={Antodaya} alt="Sunset in the mountains"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Antodaya</div>
+          <p className="text-gray-700 text-base">
+           Antyodaya is conducted on eve on Children’s Day within the college campus. Antyodaya is conducted for an all-round learning experience filled with fun! 
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#event</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#students</span>
+        </div>
+      </div>
+
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src={Antodaya} alt="Sunset in the mountains"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Antodaya</div>
+          <p className="text-gray-700 text-base">
+           Antyodaya is conducted on eve on Children’s Day within the college campus. Antyodaya is conducted for an all-round learning experience filled with fun! 
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#event</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#students</span>
+        </div>
+      </div>
+
+
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src={Antodaya} alt="Sunset in the mountains"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Antodaya</div>
+          <p className="text-gray-700 text-base">
+           Antyodaya is conducted on eve on Children’s Day within the college campus. Antyodaya is conducted for an all-round learning experience filled with fun! 
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#event</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#students</span>
+        </div>
+      </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src={Antodaya} alt="Sunset in the mountains"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Antodaya</div>
+          <p className="text-gray-700 text-base">
+           Antyodaya is conducted on eve on Children’s Day within the college campus. Antyodaya is conducted for an all-round learning experience filled with fun! 
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#event</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#students</span>
+        </div>
+      </div>
+
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src={Antodaya} alt="Sunset in the mountains"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Antodaya</div>
+          <p className="text-gray-700 text-base">
+           Antyodaya is conducted on eve on Children’s Day within the college campus. Antyodaya is conducted for an all-round learning experience filled with fun! 
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#event</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#students</span>
+        </div>
+      </div>
+
+
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src={Antodaya} alt="Sunset in the mountains"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Antodaya</div>
+          <p className="text-gray-700 text-base">
+           Antyodaya is conducted on eve on Children’s Day within the college campus. Antyodaya is conducted for an all-round learning experience filled with fun! 
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#event</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#students</span>
+        </div>
+      </div>
+      </div>
+      
+      </>
+      )}
